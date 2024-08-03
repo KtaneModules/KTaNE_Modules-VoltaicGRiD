cd51bebbeafe00d173edda553094473b50e53cdd
Dustin Conlon <dustin@dustinconlon.com>
2024-08-03 16:28:59 -0600

    Updated Broken_Calculator, use v2

---
+++ Broken_Calculator_v2/Assets/Broken_Calculator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using KModkit;
+using Rnd = UnityEngine.Random;
+using UnityEngine.UI;
+
+
+public class Broken_Calculator : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMAudio Audio;
+
+    static int ModuleIdCounter = 1;
+    int ModuleId;
+    private bool ModuleSolved;
+
+    public KMSelectable[] Buttons;
+    public TextMesh Display;
+
+    void Awake()
+    { //Avoid doing calculations in here regarding edgework. Just use this for setting up buttons for simplicity.
+        ModuleId = ModuleIdCounter++;
+        GetComponent<KMBombModule>().OnActivate += Activate;
+
+        foreach (KMSelectable obj in Buttons)
+        {
+            obj.OnInteract += delegate () { ButtonPress(obj); return false; };
+        }
+    }
+
+    void Next()
+    {
+        
+    }
+
+    void CompleteStep()
+    {
+        
+    }
+
+    void ButtonPress(KMSelectable origin)
+    {
+        
+    }
+
+    void OnDestroy()
+    { //Shit you need to do when the bomb ends
+
+    }
+
+    void Activate()
+    { //Shit that should happen when the bomb arrives (factory)/Lights turn on
+        Next();
+    }
+
+    void Start()
+    { //Shit that you calculate, usually a majority if not all of the module
+
+    }
+
+    void Update()
+    { //Shit that happens at any point after initialization
+
+    }
+
+    void Solve()
+    {
+        GetComponent<KMBombModule>().HandlePass();
+    }
+
+    void Strike()
+    {
+        GetComponent<KMBombModule>().HandleStrike();
+    }
+
+#pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+#pragma warning restore 414
+
+    IEnumerator ProcessTwitchCommand(string Command)
+    {
+        yield return null;
+    }
+
+    IEnumerator TwitchHandleForcedSolve()
+    {
+        yield return null;
+    }
+}
